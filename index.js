f9cf47e8b9fe876bc55076a50ed37128afc58094
Alekseev Aleksey <a.l.e.xseev@bk.ru>
2024-10-17 19:26:34 +0300

    Написал код дл отображения в ленте, а также сделал рефакторинг кода

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-const
+const titleInputNode = document.querySelector('#js-title-input');
+const newPostBtnNode = document.querySelector('#js-publication-btn');
+const postNode = document.querySelector('#js-add-post');
+
+let post = '';
+
+
+newPostBtnNode.addEventListener('click', function () {
+    //  Запись в переменную значение функции getPostFromUser
+    const PostFromUser = getPostFromUser();
+
+    //  Добавление в глобальную переменную post значение getPostFromUser, так как переменная post это не тоже самое, что и post
+    setPost(PostFromUser);
+
+    //  Добавление поста в Ленту постов
+    renderPost()
+})
+
+function getPostFromUser() {
+    post = titleInputNode.value;
+
+    return post;
+}
+
+function setPost(newPost) {
+    post = newPost;
+}
+
+function renderPost() {
+    postNode.innerText = post;
+}
