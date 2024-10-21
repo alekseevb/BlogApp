ef1e63eefaf082e36aebec53e5c7eb808693d0b3
Алексей Алексеев <alexseev617@gmail.com>
2024-10-21 10:55:20 +0300

    Закончил разработку приложения

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const titleInputNode = document.querySelector('#js-title-input');
+const textInputNode = document.querySelector('#js-text-input');
 const newPostBtnNode = document.querySelector('#js-publication-btn');
 const postNode = document.querySelector('#js-add-post');
+const validationMessage = document.getElementById('js-validationMessage')
 
-let post = '';
+const posts = [];
+
+const TITLE_VALIDATION_LIMIT = 10;
+const TEXT_VALIDSTION_LIMIT = 20
 
 
 newPostBtnNode.addEventListener('click', function () {
@@ -10,24 +15,84 @@ newPostBtnNode.addEventListener('click', function () {
     const PostFromUser = getPostFromUser();
 
     //  Добавление в глобальную переменную post значение getPostFromUser, так как переменная post это не тоже самое, что и post
-    setPost(PostFromUser);
+    addPost(PostFromUser);
 
     //  Добавление поста в Ленту постов
     renderPost()
+
 })
 
-function getPostFromUser() {
-    post = titleInputNode.value;
+titleInputNode.addEventListener('input', function () {
+    validation();
+})
 
-    return post;
+textInputNode.addEventListener('input', function () {
+    validation();
+})
+
+function validation() {
+    const titleLength = titleInputNode.value.length;
+    const textleLength = textInputNode.value.length;
+
+    if (titleLength > TITLE_VALIDATION_LIMIT) {
+        validationMessage.innerText = `Длина заголовка не должна превышать ${TITLE_VALIDATION_LIMIT} символов`;
+        validationMessage.classList.remove('blog__validationMessage_hidden')
+        newPostBtnNode.setAttribute('disabled', '')
+        return;
+    }
+
+    if (textleLength > TEXT_VALIDSTION_LIMIT) {
+        validationMessage.innerText = `Длина заголовка не должна превышать ${TEXT_VALIDSTION_LIMIT} символов`;
+        validationMessage.classList.remove('blog__validationMessage_hidden')
+        newPostBtnNode.setAttribute('disabled', '')
+        return;
+    }
+
+    newPostBtnNode.removeAttribute('disabled');
+    validationMessage.classList.add('blog__validationMessage_hidden')
 }
 
-function setPost(newPost) {
-    post = newPost;
+function getPostFromUser() {
+    const title = titleInputNode.value;
+    const text = textInputNode.value;
+
+    return {
+        title: title,
+        text: text,
+    };
+}
+
+function addPost({ title, text }) {
+
+    const currentDate = new Date();
+    const dt = `${currentDate.getDay()}.${currentDate.getMonth()}.${currentDate.getFullYear()} ${currentDate.getHours()}:${currentDate.getMinutes()}`
+
+    posts.push({
+        dt,
+        title,
+        text
+    });
+}
+
+function getPosts() {
+    return posts;
 }
 
 function renderPost() {
-    postNode.innerText = post;
-}
+    const posts = getPosts();
+    console.log(posts);
 
-//йоу
+    let postsHTML = '';
+
+    posts.forEach(post => {
+        postsHTML += `
+            <div class='post'>
+                <p class='post__data'>${post.dt}</p>
+                <p class='post__title'>${post.title}</p>
+                <p class='post__text'>${post.text}</p>
+            </div>
+`
+    });
+
+    postNode.innerHTML = postsHTML;
+}
